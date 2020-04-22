297a4e72064822598e061668a88e7fbab32e78ce
mfuller45 <ful19015@byui.edu>
2020-04-22 16:31:34 -0700

    master

--- js/date.js
+++ js/date.js
@@ -1,36 +1,14 @@
-var today = new Date();
-var year = today.getFullYear();
-var lastModified = new Date(document.lastModified);
+let date = new Date();
 
-let daysInMonth = createNumArray(0, 32);
-let months = createNumArray(0, 13);
-let hours = createNumArray(0, 24);
-let min = createNumArray(0, 60);
-let sec = createNumArray(0, 60);
-
-var modDay, modMonth, modYear, modHour, modMin, modSec;
-
-modDay = daysInMonth[lastModified.getDate()];
-modMonth = months[lastModified.getMonth()];
-modYear = lastModified.getFullYear();
-modHour = hours[lastModified.getHours()];
-modMin = min[lastModified.getMinutes()];
-modSec = sec[lastModified.getSeconds()];
-
-function copyYear() {
-    document.getElementById("copyright").innerHTML = " " + year;
-    document.getElementById("lastUpdate").innerHTML = "Last Updated: " + modMonth + "/" + modDay + "/" + modYear + " " + modHour + ":" + modMin + ":" + modSec;
-};
-
-function createNumArray(start, count) {
-    let array = [];
-    for (var i = start; i < count; ++i) {
-        if (i < 10) {
-            array[i] = "0" + i;
-        } else {
-            array[i] = "" + i + "";
-        }
-
+function writedate() {
+    // MM/DD/YYYY HH:MM:SS
+    let hour = date.getHours();
+    var hourText = hour;
+    var timeOfDay = 'AM';
+    if (hour > 12) {
+        hourText = hour - 12;
+        timeOfDay = 'PM';
     }
-    return array;
-};
+    document.getElementById("copyright").innerHTML = date.getFullYear();
+    document.getElementById("lastUpdate").innerHTML = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear() + ' ' + hourText + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + timeOfDay;
+}
