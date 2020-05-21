a5b0e82178780b2a02fa1f5e0768f7ead8ccc926
mfuller45 <ful19015@byui.edu>
2020-05-20 18:05:51 -0700

    master

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -5,13 +5,13 @@ function toggleMenu() {
 const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var d = new Date();
-var day = d.getDay() 
+var day = d.getDay(); 
 var date = d.getDate();
 var month = d.getMonth();
 var year = d.getFullYear();
 var dateStr = day1[day] + ', ' + date + " " + month1[month] + " " + year;
 document.getElementById("dates").innerHTML = dateStr;
-document.getElementById("copyright").innerHTML = year
+document.getElementById("copyright").innerHTML = year;
 
 function announce() {
   if (day1[day] == 'Friday') {
