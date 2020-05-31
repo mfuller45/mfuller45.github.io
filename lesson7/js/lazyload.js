307dcb9e6c22758b30cbdc7f832a8d2dc153790f
mfuller45 <ful19015@byui.edu>
2020-05-30 20:40:17 -0700

    master

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -8,7 +8,7 @@ const loadImages = (image) => {
 
 const imgOptions = {
   threshold: 0,
-  rootMargin: "0px 0px -300px 0px"
+  rootMargin: "0px 0px 50px 0px"
 };
 
 if('IntersectionObserver' in window) {
