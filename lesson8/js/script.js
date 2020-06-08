21cb25ddb7f1e6d744498ca861e56133478e3083
mfuller45 <ful19015@byui.edu>
2020-06-07 19:11:51 -0700

    master

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -14,10 +14,3 @@ document.getElementById("dates").innerHTML = dateStr;
 document.getElementById("copyright").innerHTML = year;
 
 
-if (day1[day] == 'Friday') {
-    document.getElementById("announce").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-    document.getElementById("announce").style.display = "block";
-}
-else {
-    document.getElementById("announce").style.display = "none";
-}
