e330b7ad5f8e4f6c6c2b35bf0b4d86406c60b96d
mfuller45 <ful19015@byui.edu>
2020-06-13 16:21:15 -0700

    master

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,13 +1,12 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
-const prophets = jsonObject['prophets'];
-
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
     for (let i = 0; i < prophets.length; i++ ) {  
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
