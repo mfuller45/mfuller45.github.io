f1c0ebb0a901fb7788eddbfe8aff4fbda00dc290
mfuller45 <ful19015@byui.edu>
2020-06-21 15:36:44 -0700

    master

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -23,7 +23,7 @@ else {
 }
 
 // Current Weather for Weather Summary
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1"
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1"
 
 fetch(apiURL)
   .then((response) => response.json())
