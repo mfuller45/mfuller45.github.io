fd09c59a4567c00c9eec45fd9a629d19be08d7f9
mfuller45 <ful19015@byui.edu>
2020-06-27 07:59:56 -0700

    master

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -38,27 +38,4 @@ fetch(apiURL)
     document.getElementById('windspeed').innerHTML = Math.round(town.wind.speed);
 });
 
-//Get days of the week for 5 day forecast table
 
-
-//5 day Forecast
-const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1"
-
-fetch(apiURL_forecast)
-    .then(response => response.json())
-    .then((jsObject) => {
-        console.log(jsObject);
-        const forecastData = jsObject.list.filter((element)=>element.dt_txt.includes('18:00:00'));
-
-	console.log(forecastData);
-
-	const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
-
-    let day = 0;
-	forecastData.forEach(forecast => {
-	  let x = new Date(forecast.dt_txt);
-	  document.getElementById('temp1').textContent = Math.round(forecastData[day].main.temp) + ' °F';
-	  document.getElementById('day1').textContent = weekdays[x.getDay()];
-	  day++;	  
-	});
-});
