76592e5573e8565cd039b90c16226a735efec3f6
mfuller45 <ful19015@byui.edu>
2020-07-18 20:13:37 -0700

    master

--- final/js/rentals.js
+++ final/js/rentals.js
@@ -1,5 +1,20 @@
-const url = "https://mfuller45.github.io/final/data/scoots.json";
+function toggleMenu() {
+  document.getElementsByClassName("nav_bar")[0].classList.toggle("responsive");
+ }
 
+const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var d = new Date();
+var day = d.getDay(); 
+var date = d.getDate();
+var month = d.getMonth();
+var year = d.getFullYear();
+var dateStr = day1[day] + ', ' + date + ' ' + month1[month] + ' ' + year;
+document.getElementById("dates").innerHTML = dateStr;
+document.getElementById("copyright").innerHTML = year;
+
+
+const url = "https://mfuller45.github.io/final/data/scoots.json";
 fetch(url)
   .then((response) => response.json())
     .then((data) => {
